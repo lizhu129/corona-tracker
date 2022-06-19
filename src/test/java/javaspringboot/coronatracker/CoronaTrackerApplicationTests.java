077df004be3c0cdca6caa08a3ecc9b97f6ffa72f
lizhu129 <lizhu129@gmail.com>
2022-06-19 14:56:49 +0200

    new changes

---
+++ src/test/java/javaspringboot/coronatracker/CoronaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package javaspringboot.coronatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
